cfe47dd03c33c855fedc72744096906dfd2ad4dd
Marcin Szepczyński <szepczynski@gmail.com>
2020-05-22 22:00:42 +0200

    support post method

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,51 @@ app.get('/', async (req, res) => {
   const credential = req.query.credential as string | null;
   const format = req.query.format as string | null || 'json';
 
-  return handle({
-    url,
-    username,
+  if (format !== 'json' && format !== 'flag' && format !== 'http-status') {
+    return sendJSONResponse(res, 400, {
+      error: `Invalid format. Use 'json', 'flag', or 'http-status'`
+    });
+  }
+
+  if (!url) {
+    return sendJSONResponse(res, 400, {
+      error: 'No url provided'
+    });
+  }
+
+  if (whitelist) {
+    const allowedUrls = whitelist.split(';');
+    if (allowedUrls.indexOf(url) === -1) {
+      logger.debug('Blocked health-check for non-whitelisted url', {url, username, credential});
+      return sendJSONResponse(res, 403, {
+        error: 'The provided url is not whitelisted'
+      });
+    }
+  }
+
+  logger.log('debug', `Performing health-check`, {url, username, credential});
+
+  const turnServerAvailable = await isTurnServerAvailable({
+    urls: url,
     credential,
-    format,
-    res
-  })
+    username
+  });
+
+  logger.log('debug', `Successfully performed health-check`, {url, username, credential, turnServerAvailable});
+
+  // Respond in the requested format
+  switch (format) {
+    case 'flag':
+      return res.send(turnServerAvailable ? '1' : '0');
+    case 'json':
+      return sendJSONResponse(res, 200, {
+        available: turnServerAvailable
+      });
+    case 'http-status':
+      return res.sendStatus(turnServerAvailable ? 200 : 418);
+    default:
+      throw Error('Unknown format!');
+  }
 });
 
 app.post('/', async (req, res) => {
@@ -33,21 +71,6 @@ app.post('/', async (req, res) => {
   const username = req.body.username as string | null;
   const credential = req.body.credential as string | null;
   const format = req.body.format as string | null || 'json';
-
-  return handle({
-    url,
-    username,
-    credential,
-    format,
-    res
-  })
-});
-
-// Start server
-app.listen(port, () => logger.log('info', `Server started at port ${port}`));
-
-function handle(data: { url?: string, username?: string, credential?: string, format?: string, res: any }) {
-  let {url, username, credential, format, res} = data;
 
   if (format !== 'json' && format !== 'flag' && format !== 'http-status') {
     return sendJSONResponse(res, 400, {
@@ -94,7 +117,10 @@ function handle(data: { url?: string, username?: string, credential?: string, fo
     default:
       throw Error('Unknown format!');
   }
-}
+});
+
+// Start server
+app.listen(port, () => logger.log('info', `Server started at port ${port}`));
 
 function sendJSONResponse(res: any, status: number, json: any) {
   res.statusCode = status;
